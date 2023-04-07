a2c04996298e0ccc3f045bdc31116d3210078f52
pakgilang <74170689+pakgilang@users.noreply.github.com>
2023-04-07 16:13:09 +0700

    Update iklan.js

--- iklan.js
+++ iklan.js
@@ -22,12 +22,9 @@ lazyOnScroll();});}}(jQuery);
 /*! cek kata #dewasa */
 const entryContent=document.querySelector(".entry-content");if(entryContent.innerText.toLowerCase().includes("#dewasa")){const a=document.createElement("div");a.classList.add("popup");const b=document.createElement("p");b.innerText="Apakah Anda sudah berumur 18+?",a.appendChild(b);const c=document.createElement("button");c.innerText="Sudah",c.addEventListener("click",()=>{a.remove()}),a.appendChild(c);const d=document.createElement("button");d.innerText="Belum",d.addEventListener("click",()=>{window.location.href="https://www.hiburan.web.id/"}),a.appendChild(d),document.body.appendChild(a)}
 
-/*! ubah gambar ke webp */
+/*! alt dan title */
 const images=document.querySelectorAll('img');images.forEach((e=>{const t=e.getAttribute("src"),n=t.substring(t.lastIndexOf("/")+1,t.lastIndexOf(".")).replace(/-/g," ");e.setAttribute("alt",n),e.setAttribute("title",n)}));
                                               
-/*! auto alt dan title berdasarkan nama */
-document.addEventListener("DOMContentLoaded",function(){for(var t=document.getElementsByTagName("img"),e=0;e<t.length;e++){var n=t[e].getAttribute("src");-1!==n.lastIndexOf("/")&&(n=(n=(n=n.substring(n.lastIndexOf("/")+1)).substring(0,n.lastIndexOf("."))).replace(/-/g," "),t[e].setAttribute("alt",n),t[e].setAttribute("title",n))}});
-
 /*! Table of Contents | v0.4.0 - https://github.com/ndabas/toc */
 !function(t){"use strict";var n=function(n){return this.each(function(){var e,i,a=t(this),o=a.data(),c=[a],r=this.tagName,d=0;e=t.extend({content:"body",headings:"h1,h2,h3"},{content:o.toc||void 0,headings:o.tocHeadings||void 0},n),i=e.headings.split(","),t(e.content).find(e.headings).attr("id",function(n,e){return e||function(t){0===t.length&&(t="?");for(var n=t.replace(/\s+/g,"_"),e="",i=1;null!==document.getElementById(n+e);)e="_"+i++;return n+e}(t(this).text())}).each(function(){var n=t(this),e=t.map(i,function(t,e){return n.is(t)?e:void 0})[0];if(e>d){var a=c[0].children("li:last")[0];a&&c.unshift(t("<"+r+"/>").appendTo(a))}else c.splice(0,Math.min(d-e,Math.max(c.length-1,0)));t("<li/>").appendTo(c[0]).append(t("<a/>").text(n.text()).attr("href","#"+n.attr("id"))),d=e})})},e=t.fn.toc;t.fn.toc=n,t.fn.toc.noConflict=function(){return t.fn.toc=e,this},t(function(){n.call(t("[data-toc]"))})}(window.jQuery);
 
